2639ea87e5621818eeb3a6616f2986f342033e89
flamurbakiu <flamurbakiu.15@gmail.com>
2022-02-10 21:31:49 +0100

    Initial version

--- js/main.js
+++ js/main.js
@@ -46,9 +46,9 @@ jQuery(document).ready(function ($) {
     pagination: true,
     paginationNumbers: false,
     autoPlay: 5000, //Set AutoPlay to 3 seconds
-    items: 2, //10 items above 1000px browser width
-    itemsDesktop: [1000, 2], //5 items between 1000px and 901px
-    itemsDesktopSmall: [900, 1], // betweem 900px and 601px
+    items: 3, //10 items above 1000px browser width
+    itemsDesktop: [1000, 3], //5 items between 1000px and 901px
+    itemsDesktopSmall: [900, 2], // betweem 900px and 601px
     itemsTablet: [600, 1], //2 items between 600 and 0
     itemsMobile: false, // itemsMobile disabled - inherit from itemsTablet option
   });
